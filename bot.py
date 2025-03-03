03d562f1e7130136eae503876053219f2c9ad9c3
Gowtham380 <113359765+Gowtham380@users.noreply.github.com>
2025-03-04 01:56:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telethon import TelegramClient, events
 # Replace with your actual API credentials
 API_ID = 21953115  # Replace with your API ID
 API_HASH = edfd34085e9ba51303155f75d77b09ae
-BOT_TOKEN = 6546811614:AAGdwWWHWLcqaneUnM5OpJ12tB0RgKIRzbY
+BOT_TOKEN = 6546811614:AAEbnyecTpv_3fs7VRj_uLWb8bmr12NzSK4
 
 # Initialize the bot client
 client = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
